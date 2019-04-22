0ce95699183ea0bc55c3c6bee5459e3da4e6a845
wanghuanqing <1097864491@qq.com>
2019-04-22 20:25:00 +0800

    mybatis使用示例提交

--- springMVC/src/main/java/com/whq/LoginAction.java
+++ springMVC/src/main/java/com/whq/LoginAction.java
@@ -1,9 +1,17 @@
 package com.whq;
 
+import com.whq.dao.UserinfoMapper;
+import com.whq.entity.Userinfo;
+import com.whq.entity.UserinfoExample;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author: wanghuanqing
@@ -13,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class LoginAction implements InitializingBean {
+    @Autowired
+    private UserinfoMapper userinfoMapper;
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -21,9 +31,26 @@ public class LoginAction implements InitializingBean {
 
     @RequestMapping("/login")
     public String login(@ModelAttribute("a") Users users){
+        int i = 0;
+        List<Userinfo> userinfoList = new ArrayList<Userinfo>();
         System.out.println("方式2接收到的用户名：" + users.getUsername());
         System.out.println("方式2接收到的密码：" + users.getPassword());
+        userinfoList = this.selectUserInfo();
+        Iterator<Userinfo> userinfoIterator = userinfoList.iterator();
+        while (userinfoIterator.hasNext()){
+            ++i;
+            Userinfo userinfo = userinfoIterator.next();
+            System.out.println("存在的用户名：" + userinfo.getUsername());
+            if(i >= 10){
+                break;
+            }
+        }
         System.out.println("Login success!");
         return "success";
+    }
+
+    public List<Userinfo> selectUserInfo(){
+        UserinfoExample userinfoExample = new UserinfoExample();
+        return userinfoMapper.selectByExample(userinfoExample);
     }
 }
